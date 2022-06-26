edae5792c2be763257c5e534f9f3fcb9c64387e9
Saeed Nikdel <1991saeed@gmail.com>
2022-06-26 20:31:39 +0330

    right menu created

--- src/components/RightMenu.jsx
+++ src/components/RightMenu.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import data from '../data';
+import { QueueMusicOutlined } from '@mui/icons-material';
 const RightMenu = ({}) => {
   return (
     <div
       className={`flex flex-col h-screen overflow-auto pb-28 space-y-1 text-lg font-semibold bg-slate-900 text-gray-200`}>
       <h1 className=" sticky top-0 bg-slate-900  pt-16 pb-4 px-2">
+        <QueueMusicOutlined />
         Now Playing
       </h1>
       {data.map((item) => (
