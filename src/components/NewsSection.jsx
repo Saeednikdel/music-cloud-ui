11bc8f412acc5261f4b5c731bf77a7f1bba35516
Saeed Nikdel <1991saeed@gmail.com>
2022-06-27 00:12:07 +0330

    remove reverse

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -4,7 +4,7 @@ import data from '../data';
 const NewsSection = () => {
   return (
     <div className="overflow-auto whitespace-nowrap p-4 space-x-4">
-      {data.reverse().map((item) => (
+      {data.map((item) => (
         <NewCard key={DataTransferItemList.title} item={item} />
       ))}
     </div>
