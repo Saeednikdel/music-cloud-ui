64a9912e398cf55b5a5d19acf608ae0cd82ddf96
Saeed Nikdel <1991saeed@gmail.com>
2022-06-26 20:17:58 +0330

    reverse order

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -4,7 +4,7 @@ import data from '../data';
 const NewsSection = () => {
   return (
     <div className="overflow-auto whitespace-nowrap p-4 space-x-4">
-      {data.map((item) => (
+      {data.reverse().map((item) => (
         <NewCard key={DataTransferItemList.title} item={item} />
       ))}
     </div>
