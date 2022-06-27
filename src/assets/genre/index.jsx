c55438f884bc2c65cdc0d9620a05bcdea3f2062a
Saeed Nikdel <1991saeed@gmail.com>
2022-06-27 16:37:16 +0330

    add genre

---
+++ src/assets/genre/index.jsx
@@ -0,0 +1,8 @@
+export { default as classical } from './classical.jpeg';
+export { default as country } from './country.jpg';
+export { default as electronic } from './electronic.jpg';
+export { default as hiphop } from './hiphop.jpg';
+export { default as jazz } from './jazz.jpg';
+export { default as pop } from './pop.jpg';
+export { default as rap } from './rap.jpg';
+export { default as rock } from './rock.jpg';
---
+++ src/genres.js
@@ -0,0 +1,36 @@
+import * as genre from './assets/genre';
+const genres = [
+  {
+    title: 'rock',
+    image: genre.rock,
+  },
+  {
+    title: 'rap',
+    image: genre.rap,
+  },
+  {
+    title: 'pop',
+    image: genre.pop,
+  },
+  {
+    title: 'jazz',
+    image: genre.jazz,
+  },
+  {
+    title: 'hiphop',
+    image: genre.hiphop,
+  },
+  {
+    title: 'electronic',
+    image: genre.electronic,
+  },
+  {
+    title: 'country',
+    image: genre.country,
+  },
+  {
+    title: 'classical',
+    image: genre.classical,
+  },
+];
+export default genres;
