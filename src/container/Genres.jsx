942863e7e25caeaf6e77a56608d38a72ba681d76
Saeed Nikdel <1991saeed@gmail.com>
2022-07-02 20:14:11 +0330

    key

--- src/container/Genres.jsx
+++ src/container/Genres.jsx
@@ -3,8 +3,10 @@ import genres from '../genres';
 const Genres = () => {
   return (
     <div className="mb-24">
-      {genres.map((item) => (
-        <div className="flex py-2 px-6 text-xl font-semibold space-x-4 hover:cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700">
+      {genres.map((item, i) => (
+        <div
+          key={i}
+          className="flex py-2 px-6 text-xl font-semibold space-x-4 hover:cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700">
           <img
             alt={item.title}
             className=" h-20 w-20 object-cover rounded-lg"
--- src/container/PlayLists.jsx
+++ src/container/PlayLists.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { Add, PlaylistPlay } from '@mui/icons-material';
 const PlayLists = () => {
-  const list = [
-    'Recently added',
-    'Recently played',
-    'Most played',
-    'Not played',
-  ];
+  const list = ['Recently added', 'Recently played', 'Most played'];
   return (
     <div className="mb-24 mt-4">
       <div className="flex py-2 px-6 text-xl font-semibold space-x-4 hover:cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700">
         <Add fontSize="large" />
         <h2>Create New PlayList</h2>
       </div>
-      {list.map((item) => (
-        <div className="flex py-2 px-6 text-xl font-semibold space-x-4 hover:cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700">
+      {list.map((item, i) => (
+        <div
+          key={i}
+          className="flex py-2 px-6 text-xl font-semibold space-x-4 hover:cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-700">
           <PlaylistPlay fontSize="large" />
           <h2>{item}</h2>
         </div>
