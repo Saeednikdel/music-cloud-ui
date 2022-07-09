0b30abe09954e73a0ec6aa048a058682d5e6cdd1
Saeed Nikdel <1991saeed@gmail.com>
2022-07-09 22:58:50 +0330

    remove like

--- src/components/MoreMenu.jsx
+++ src/components/MoreMenu.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   Share,
   Report,
-  FavoriteBorder,
   Add,
   Download,
   LibraryMusic,
@@ -13,13 +12,13 @@ const MoreMenu = ({ showMore, currentSong, index }) => {
     <div
       className={`${
         showMore ? 'block' : 'hidden'
-      } absolute bg-white dark:bg-slate-900 border bottom-8 right-0 border-gray-300 dark:border-gray-600 mb-2 p-2 rounded-lg z-10 shadow-lg w-60 space-y-1`}>
+      } absolute bg-white dark:bg-slate-900 border bottom-8 right-0 border-gray-300 dark:border-gray-600 mb-2 p-2 rounded-lg z-10 shadow-lg w-60 space-y-1 translate-x-52`}>
       <div
         onClick={() =>
           navigator.share({
             title: 'Music Cloud',
             text: 'Music Cloud',
-            url: 'https://music-cloud-ui.vercel.app/',
+            url: `${process.env.REACT_APP_API_URL}/p/${index}`,
           })
         }
         className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
@@ -38,10 +37,6 @@ const MoreMenu = ({ showMore, currentSong, index }) => {
           <h1 className="text-xl">Download</h1>
         </div>
       </a>
-      <div className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
-        <FavoriteBorder />
-        <h1 className="text-xl">Favorite</h1>
-      </div>
       <div className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
         <Add />
         <h1 className="text-xl">Add to playlist</h1>
