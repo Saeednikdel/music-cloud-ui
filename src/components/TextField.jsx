dc01ab1d8ca06dbafddb3e91b65b0715538cf0ff
Saeed Nikdel <1991saeed@gmail.com>
2022-07-10 22:27:38 +0330

    remove text red

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -28,8 +28,8 @@ const TextField = ({
         onChange={(e) => onChange(e)}
         className={`bg-gray-50 border dark:bg-gray-600 ${
           error
-            ? 'text-red-600 border-red-600 dark:text-red-500 dark:border-red-500'
-            : 'text-gray-900 dark:text-white border-gray-300 dark:border-gray-500 '
+            ? ' border-red-600 dark:border-red-500'
+            : ' border-gray-300 dark:border-gray-500 '
         } text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 `}
         placeholder={placeholder}
         minLength={minLength}
