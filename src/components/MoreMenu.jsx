a068bb8509639c2806c91dcd09f334ea2a13e16b
Saeed Nikdel <1991saeed@gmail.com>
2022-07-19 19:31:07 +0330

    fix width of popup

--- src/components/MoreMenu.jsx
+++ src/components/MoreMenu.jsx
@@ -32,7 +32,7 @@ const MoreMenu = ({
       dir={direction}
       className={`${
         showMore ? 'block' : 'hidden'
-      } absolute bg-white dark:bg-slate-900 border bottom-8 right-0 border-gray-300 dark:border-gray-600 mb-2 p-2 rounded-lg z-10 shadow-lg w-60 space-y-1 translate-x-52`}>
+      } absolute bg-white dark:bg-slate-900 border bottom-8 right-0 border-gray-300 dark:border-gray-600 mb-2 p-2 rounded-lg z-10 shadow-lg w-72 space-y-1 translate-x-64`}>
       <div
         onClick={() =>
           navigator.share({
@@ -41,42 +41,42 @@ const MoreMenu = ({
             url: `${process.env.REACT_APP_API_URL}/p/${index}`,
           })
         }
-        className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
+        className="flex px-2 py-1 items-center hover:cursor-pointer active:text-blue-600">
         <Share />
-        <h1 className="text-xl">{translate('Share')}</h1>
+        <h1 className="text-xl mx-2">{translate('Share')}</h1>
       </div>
       {/* <Link to={`/createcard/${index}/`}>
-        <div className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
+        <div className="flex px-2 py-1 items-center hover:cursor-pointer active:text-blue-600">
           <LibraryMusic />
           <h1 className="text-xl">Lyrics card</h1>
         </div>
       </Link> */}
       {user && user.name && user.name === currentSong.user_name && (
         <Link to={`/edit/${index}/`}>
-          <div className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
+          <div className="flex px-2 py-1 items-center hover:cursor-pointer active:text-blue-600">
             <Edit />
-            <h1 className="text-xl">{translate('Edit track')}</h1>
+            <h1 className="text-xl mx-2">{translate('Edit track')}</h1>
           </div>
         </Link>
       )}
 
       <a href={currentSong.url} download={currentSong.title + '[Music Cloud]'}>
-        <div className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
+        <div className="flex px-2 py-1 items-center hover:cursor-pointer active:text-blue-600">
           <Download />
-          <h1 className="text-xl">{translate('Download')}</h1>
+          <h1 className="text-xl mx-2">{translate('Download')}</h1>
         </div>
       </a>
       {user && (
         <div
           onClick={handleAddtoplaylist}
-          className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
+          className="flex px-2 py-1 items-center hover:cursor-pointer active:text-blue-600">
           <Add />
-          <h1 className="text-xl">{translate('Add to playlist')}</h1>
+          <h1 className="text-xl mx-2">{translate('Add to playlist')}</h1>
         </div>
       )}
-      <div className="flex px-2 py-1 space-x-2 items-center hover:cursor-pointer active:text-blue-600">
+      <div className="flex px-2 py-1 items-center hover:cursor-pointer active:text-blue-600">
         <Report />
-        <h1 className="text-xl">{translate('Report')}</h1>
+        <h1 className="text-xl mx-2">{translate('Report')}</h1>
       </div>
     </div>
   );
